ac097066413680807e72dc1a64c290f4baa73913
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2023-08-02 23:49:50 +0530

    Time: 7 ms (93.38%), Space: 5.8 MB (79.51%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        for(int i=s.size()-1,j=0;i>=0;i--,j++)
+        {
+            if(s[i]!=s[j])return 0;
+        }
+        return 1;
+    }
+};
