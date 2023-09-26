401009818b2d64c160d7a9c5b3c7652a5bf0fc8a
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2023-09-26 15:55:33 +0530

    Time: 2 ms (59.76%), Space: 7.2 MB (10.66%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -5,17 +5,17 @@ public:
         for(int i=0;i<s.size();i++){
             mp[s[i]] = i;
         }
-        vector<char> vis(26,false);
+        vector<char> vis(26,0);
         stack<char> st;
         for(int i=0;i<s.size();i++){
             char x = s[i];
-            if(vis[x-'a'] == true)
+            if(vis[x-'a'] == 1)
                 continue;
             while(!st.empty() && st.top()>x && mp[st.top()]>i){
-                vis[st.top()-'a'] = false;
+                vis[st.top()-'a'] = 0;
                 st.pop();
             }
-            vis[x-'a'] = true;
+            vis[x-'a'] = 1;
             st.push(x);
         }
         string ans = "";
