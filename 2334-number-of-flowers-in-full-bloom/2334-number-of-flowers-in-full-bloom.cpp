93c473745083130eb8c346c3b366ffe68adb8694
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2023-10-11 04:35:15 -0700

    Time: 238 ms (73.32%), Space: 89.3 MB (43.56%) - LeetHub

---
+++ 2334-number-of-flowers-in-full-bloom/2334-number-of-flowers-in-full-bloom.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
+        vector<int>start,end;
+        for (auto it: flowers)
+        {
+            start.push_back(it[0]);
+            end.push_back(it[1]);
+        }
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
+        vector<int>ans;
+        for (int i=0;i<people.size();i++)
+        {
+            int s=upper_bound(start.begin(),start.end(),people[i])-start.begin();
+            int e=lower_bound(end.begin(),end.end(),people[i])-end.begin();
+            ans.push_back(s-e);
+        }
+        return ans;
+    }
+};
