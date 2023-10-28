fa2a9e30015bfe201704c7402a65ad0e71b27c14
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2023-10-28 16:21:35 -0700

    Time: 13 ms (72.3%), Space: 6.5 MB (78.85%) - LeetHub

---
+++ 1332-count-vowels-permutation/1332-count-vowels-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        vector<long> dp0(k, 1);
+        vector<long> dp1(k);
+
+        for (int x = 2; x <= n; x++) {
+            dp1[a] = (dp0[e] + dp0[i] + dp0[u]) % MOD;  // ea, ia, ua
+            dp1[e] = (dp0[a] + dp0[i]) % MOD;           // ae, ie
+            dp1[i] = (dp0[e] + dp0[o]) % MOD;           // ei, oi
+            dp1[o] = dp0[i];                            // io
+            dp1[u] = (dp0[i] + dp0[o]) % MOD;           // iu, ou
+            swap(dp0, dp1);
+        }
+        
+        return (dp0[a] + dp0[e] + dp0[i] + dp0[o] + dp0[u]) % MOD;
+    }
+    enum vowel {
+        a = 0,
+        e,
+        i,
+        o,
+        u,
+        k,
+    };
+    const int MOD = 1e9 + 7;  
+};
