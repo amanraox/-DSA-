92ca11dbd26d18c8e06450b17a9c74099ebff204
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2023-11-06 03:59:02 -0800

    Time: 0 ms (100%), Space: 6.2 MB (75.34%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        unsigned int t0=0,t1=1,t2=1,t3;
+        if(n==0)return 0;
+        else if(n==1)return 1;
+        else if(n==2)return 1;
+        n-=2;
+        while(n--)
+        {
+            t3=t0+t1+t2;
+            t0=t1,t1=t2,t2=t3;
+        }
+        return t3;
+    }
+};
