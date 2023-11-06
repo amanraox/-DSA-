de5ae2b13d63ced6004ae2311a7d0da35ebaacce
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2023-11-06 04:03:53 -0800

    Time: 2 ms (46.77%), Space: 6.2 MB (78.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,11 +3,7 @@ public:
     int climbStairs(int n) {
         int s=0,e=1,c;
         while(n--)
-        {
-            c=s+e;
-            s=e;
-            e=c;
-        }
+           c=s+e,s=e,e=c;
         return c;
     }
 };
