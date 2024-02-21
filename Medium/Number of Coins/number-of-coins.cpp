a5abd61a9bed72017f7efffc76358228aa0b0e46
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-02-22 00:23:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Coins/number-of-coins.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+	public:
+	int minCoins(vector<int> &coins, int M, int V) 
+	{ 
+	     std::vector<int> dp(V + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 1; i <= V; i++) {
+            for (int j = 0; j < M; j++) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX) {
+                    dp[i] = std::min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return (dp[V] == INT_MAX) ? -1 : dp[V];
+	} 
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int v, m;
+        cin >> v >> m;
+
+        vector<int> coins(m);
+        for(int i = 0; i < m; i++)
+        	cin >> coins[i];
+
+      
+	    Solution ob;
+	    cout << ob.minCoins(coins, m, v) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
