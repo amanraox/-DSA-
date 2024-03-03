addf329fd94c557133420f792d9524f9e52e6533
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-03-03 21:59:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Egg Dropping Puzzle/egg-dropping-puzzle.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find minimum number of attempts needed in 
+    //order to find the critical floor.
+    int eggDrop(int n, int k) 
+    {
+        vector<int> dp(n+1, 0);
+        int ans = 0;
+        while(dp[n]<k){
+            ans++;
+            for(int egg=n; egg>0; egg--){
+                dp[egg] += 1 + dp[egg-1];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking eggs and floors count
+        int n, k;
+        cin>>n>>k;
+        Solution ob;
+        //calling function eggDrop()
+        cout<<ob.eggDrop(n, k)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
