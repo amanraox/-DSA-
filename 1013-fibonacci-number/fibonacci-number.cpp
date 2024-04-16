3d4671313d9624fe3af9912c769dd9537f07deaf
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-04-17 02:29:54 +0530

    Time: 7 ms (56.28%) | Memory: 7.2 MB (34.56%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)return 0;
-        if(n<3)return 1;
+        //if(n==0)return 0;
+        if(n<2)return n;
         return fib(n-1)+fib(n-2);
     }
 };
