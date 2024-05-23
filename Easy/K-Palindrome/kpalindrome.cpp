3e68478a993185df5516abdcf3c37ed72e9d747f
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-05-23 22:18:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/K-Palindrome/kpalindrome.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int kPalindrome(string str, int n, int k)
+    {
+vector<vector<int>> dp(n, vector<int>(n, 0));
+    
+        for (int gap = 1; gap < n; ++gap) {
+            for (int i = 0, j = gap; j < n; ++i, ++j) {
+                if (str[i] == str[j]) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int v = dp[0][n-1];
+        return v <= k;    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.kPalindrome(str, n, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
