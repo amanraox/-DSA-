a6db439f8e184992d1768ed858727522f9d63713
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-04 12:06:39 +0530

    Time: 4 ms (44.24%) | Memory: 7.7 MB (88.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> a(126,0);bool c=1;int ans=s.length();
         for(auto i:s)++a[i];
         for(auto i:a)
-        if(i==0 or i%2==0)continue;
+        if(i==0 or !(i&1))continue;
         else if(c and (i&1)){c=0;}
         else --ans;
         return ans;
