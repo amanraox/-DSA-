16b100eded379fc6905f82958774cb43bb348eb9
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-04 12:06:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int> a(126,0);
-        for(auto i:s)
-        ++a[i];
-        bool c=1;
-        int ans=s.length();
+        vector<int> a(126,0);bool c=1;int ans=s.length();
+        for(auto i:s)++a[i];
         for(auto i:a)
-        if(i==0)continue;
+        if(i==0 or i%2==0)continue;
         else if(c and (i&1)){c=0;}
-        else if(!c and i&1)--ans;
+        else --ans;
         return ans;
     }
 };
