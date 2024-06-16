98786e3ec0163c493d41d9f5ef99c440340fb696
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-16 23:54:01 +0530

    Time: 3 ms (83.44%) | Memory: 13.9 MB (12.30%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long sum = 0;
+        int ans=0;
+        while(sum<=n)
+        {
+            long long y =0;
+            for(auto i:nums)
+            {
+                if(i>sum)
+                continue;
+                y+=i;
+            }
+            if(y>=sum)
+            {
+                sum = y+1;
+            }
+            else
+            {
+                ans++;
+                nums.push_back(y+1);
+            }
+        }
+
+        return ans;
+    }
+};
