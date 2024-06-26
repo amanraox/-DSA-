2228075e663f5da367a1c0779cba384f5b4db3a3
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-06-26 23:58:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge two BST 's/merge-two-bst-s.cpp
@@ -0,0 +1,175 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+
+
+// } Driver Code Ends
+/*
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    void inOrder(Node* root , vector<int>&ans){
+        if(root == NULL) return;
+        
+        inOrder(root -> left , ans);
+        ans.push_back(root->data);
+        inOrder(root -> right , ans);
+    }
+    
+    vector<int> sortedMergeArray(vector<int>& one , vector<int>& two){
+         vector<int> result;
+         int i = 0 , j = 0;
+         
+        while(i < one.size() && j < two.size()){
+            if(one[i] < two[j]){
+                result.push_back(one[i++]);
+            }else{
+                result.push_back(two[j++]);
+            }
+        }
+        
+          while(i < one.size()){
+               result.push_back(one[i++]);
+          }
+          
+           while(j < two.size()){
+               result.push_back(two[j++]);
+          }
+          
+          return result;
+    }
+    
+    vector<int> merge(Node *root1, Node *root2)
+    {
+        // Step 1 
+       vector<int> one , two;
+       inOrder(root1 , one);
+       inOrder(root2 , two);
+       
+    //   step 2
+       return sortedMergeArray(one , two);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root1 = buildTree(s);
+
+       getline(cin, s);
+       Node* root2 = buildTree(s);
+    
+       // getline(cin, s);
+       Solution obj;
+       vector<int> vec = obj.merge(root1, root2);
+       for(int i=0;i<vec.size();i++)
+            cout << vec[i] << " ";
+        cout << endl;
+       ///cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
