4e15a2c9f3e385c698f4d7263fae010047eff408
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-07-12 22:02:52 +0530

    Time: 117 ms (23.17%) | Memory: 38 MB (5.28%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -19,7 +19,7 @@ pair<int,string> solve(string s,string target,int c){
         return {ans,temp};
     }
     int maximumGain(string s, int x, int y) {
-        string ab="abaa",ba="ba";
+        string ab="abaababbab",ba="ba";
         if(x<y){
             swap(x,y);
             swap(ab,ba);
