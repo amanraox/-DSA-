02484eb012fb021a5f7cc13dbe5f90d672b87848
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-08-01 18:51:42 +0530

    Time: 8 ms (47.01%) | Memory: 17.6 MB (27.35%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int countSeniors(vector<string>& d) {
-        int ans=0;
-        for(int i=0;i<d.size();i++)
-            if(((d[i][11]-'0')*10+(d[i][12]-'0'))>60)++ans;
+        int ans = 0;
+        for (int i = 0; i < d.size(); i++)
+            if (((d[i][11] - '0') * 10 + (d[i][12] - '0')) > 60) ++ans;
         return ans;
     }
 };
