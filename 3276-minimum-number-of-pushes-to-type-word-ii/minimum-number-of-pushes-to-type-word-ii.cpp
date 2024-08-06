f6f88b2b57c842200ea08e3e17c2e93216c5f852
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-08-06 23:35:43 +0530

    Time: 74 ms (55.49%) | Memory: 27.6 MB (20.64%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -5,13 +5,9 @@ public:
         int n=w.size(),ans=0;
         for(int i=0;i<n;i++)++mp[w[i]-'a'];
         sort(mp.rbegin(),mp.rend());
-        // reverse(mp.begin(),mp.end());
         for(int i=0;i<26;i++)
         if(mp[i]==0)break;
-        else if(i<8)ans+=mp[i];
-        else if(i<16)ans+=mp[i]*2;
-        else if(i<24)ans+=mp[i]*3;
-        else ans+=mp[i]*4;
+        else ans+=mp[i]*(i/8+1);
         return ans;
     }
 };
