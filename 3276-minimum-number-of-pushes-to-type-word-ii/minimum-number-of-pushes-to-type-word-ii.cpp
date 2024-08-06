4073d84c3d8d03baa670937fcfad81849ce1a48c
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-08-07 00:02:47 +0530

    Time: 81 ms (36.51%) | Memory: 27.8 MB (14.43%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minimumPushes(string w) {
         vector<int> mp(26,0);
-        int n=w.size(),ans=0;
-        for(int i=0;i<n;i++)++mp[w[i]-'a'];
+        int ans=0;
+        for(int i=0;i<w.size();i++)++mp[w[i]-'a'];
         sort(mp.rbegin(),mp.rend());
         for(int i=0;i<26;i++)ans+=mp[i]*(i/8+1);
         return ans;
