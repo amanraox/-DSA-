61d8846cb06a48571287ce87e5edbfab1ce48a17
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-08-15 17:07:21 +0530

    Time: 56 ms (96.67%) | Memory: 86 MB (32.71%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        if(bills[0]!=5)return 0;
+        int fi=0,te=0;
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5)++fi;
+            else if(bills[i]==10)--fi,++te;
+            else 
+            {
+                if(te)--te,--fi;
+                else fi-=3;
+            }
+            if(fi<0)return 0;
+        }
+        return 1;
+    }
+};
