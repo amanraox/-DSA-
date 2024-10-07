9e6165efc4878c4b8e25191d7272cfce4c1376a6
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-10-07 19:07:38 +0530

    Time: 8 ms (42.58%) | Memory: 10.9 MB (78.35%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -6,12 +6,9 @@ public:
         {
             if(s[i]=='A'){a.push_back(1);continue;}
             else if(s[i]=='C'){a.push_back(2);continue;}
-            if(a.size())
-            {
-                if(s[i]=='B' and a.back()==1)a.pop_back(),ans++;
-                else if(s[i]=='D' and a.back()==2)a.pop_back(),ans++;
-                else a.clear();
-            }
+            else if(a.size() and s[i]=='B' and a.back()==1)a.pop_back(),ans++;
+            else if(a.size() and s[i]=='D' and a.back()==2)a.pop_back(),ans++;
+            else a.clear();
         }
         return s.length()-2*ans;
     }
