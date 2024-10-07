1d50e995f51f611ce30a594e3cdd2a4a916498bd
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-10-07 06:06:17 +0530

    Time: 12 ms (15.77%) | Memory: 10.7 MB (78.76%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minLength(string s) {
-        int n=s.length(),ans=0;
-        vector<int> a;
-        for(int i=0;i<n;i++)
+        int ans=0;vector<int> a;
+        for(int i=0;i<s.length();i++)
         {
-            if(s[i]=='A') {a.push_back(1);continue;}
+            if(s[i]=='A'){a.push_back(1);continue;}
             else if(s[i]=='C'){a.push_back(2);continue;}
-            if(a.size()){
+            if(a.size())
+            {
                 if(s[i]=='B' and a.back()==1)a.pop_back(),ans++;
                 else if(s[i]=='D' and a.back()==2)a.pop_back(),ans++;
-                else a.clear();}
+                else a.clear();
+            }
         }
-        return n-2*ans;
+        return s.length()-2*ans;
     }
 };
