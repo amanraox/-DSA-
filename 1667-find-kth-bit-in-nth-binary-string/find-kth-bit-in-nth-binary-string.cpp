5d292ddd92d0ab21b805a49efe333fad9266d032
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-10-19 22:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (87.54%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     char findKthBit(int n, int k) {
         if (k==1) return '0';
-        int sz=32-__builtin_clz(k);
-        int k0=(1<<sz)-k;
+        int k0=(1<<32-__builtin_clz(k))-k;
         if (k0==k) return '1';
         return (findKthBit(n, k0)=='0')?'1':'0';
     }
