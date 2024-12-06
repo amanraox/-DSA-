4f8ba18ae86cf27dbdcdad57126a7cdf1a5db67c
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2024-12-07 01:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (47.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n= size(nums);
-       
+        int n=size(nums);
+        bool onepresent=false;
+        // if one is missing return one 
         for(int i=0;i<n;i++){
-            int x=nums[i];
-            while(x>=1 && x<=n && x!=i+1 && nums[x-1]!=x){
-                swap(nums[x-1],nums[i]);
-                x=nums[i];
-            }
+            if(nums[i]==1)onepresent=1;
+            if(nums[i]<=0 || nums[i]>n)nums[i]=1;
         }
-        for(int i=0;i<n;i++){
-            if(nums[i] == i+1)continue;
-                return i+1;       
-            
-        }
+        if(onepresent==false)return 1;
+        nums.push_back(1);
         
+        // if x is present make element at nums[x] negative
+        for(int i=0;i<n;i++){
+            int ind=abs(nums[i]);
+            if(nums[ind]>0)nums[ind]=-1*nums[ind];
+        }
+         
+        // if a index is not negative that mean that element   was  not present in array    
+        for(int i=1;i<=n;i++){
+            if(nums[i]>0)return i;
+        }
         return n+1;
     }
 };
