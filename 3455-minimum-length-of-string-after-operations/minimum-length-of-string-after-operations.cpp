453ce160cd12002eb56f253a4a56347b04a6150d
Aman Umrao <67886237+amanraox@users.noreply.github.com>
2025-01-13 22:11:37 +0530

    Time: 24 ms (59.68%) | Memory: 32.1 MB (58.06%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        vector<int> a(26,0);int n=s.length();
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            ++a[s[i]-'a'];
-        }
+        vector <int> a(26,0);int ans=0;
+        for(int i=0;i<s.length();i++)++a[s[i]-'a'];
         for(int i=0;i<26;i++)
         {
-            if(a[i]&1)ans++;
-            if(a[i]%2==0 and a[i]>0) ans+=2;
+            if(a[i]>0 and a[i]&1)++ans;
+            else if(a[i]>0)ans+=2;
         }
         return ans;
     }
